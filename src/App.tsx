138e1a098b0fcfad2fe4d24fb92fe44e08b14510
Wahid Hashem <hashemwahid321@gmail.com>
2025-10-24 19:22:02 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 function App() {
   return (
-    <>
+    <div className="bg-neutral-100">
       <Button>Click me</Button>
-    </>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>Card content</CardContent>
+        <CardFooter>Card footer</CardFooter>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>Card content</CardContent>
+        <CardFooter>Card footer</CardFooter>
+      </Card>
+    </div>
   );
 }
 
